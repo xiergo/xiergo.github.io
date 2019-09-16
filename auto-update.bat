3e05191aa8105c8aa851f22d6c1e2ea7bbb502d7
xiergo <39729490+xiergo@users.noreply.github.com>
2019-09-16 10:40:30 +0800

    'gadf'

--- auto-update.bat
+++ auto-update.bat
@@ -1,5 +1,9 @@
+cd "D:\Users\xiergo\Documents\GitHub\mysite"
+hugo
+xcopy D:\Users\xiergo\Documents\GitHub\mysite\public D:\Users\xiergo\Documents\GitHub\xiergo.github.io /eiy
+cd D:\Users\xiergo\Documents\GitHub\xiergo.github.io
 git add .
-git commit -m 'whoisdaday'
+git commit -m 'gadf'
 git push
 
 pause
