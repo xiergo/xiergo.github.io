d656eaac129922ab87ce49973ed33bb9940d0ff1
xiergo <39729490+xiergo@users.noreply.github.com>
2019-09-17 19:19:45 +0800

    'gadf'

--- auto-update.bat
+++ auto-update.bat
@@ -1,6 +1,6 @@
-cd "D:\Users\xiergo\Documents\GitHub\mysite"
+cd "D:\Users\xiergo\Documents\GitHub\mysite-even"
 hugo
-xcopy D:\Users\xiergo\Documents\GitHub\mysite\public D:\Users\xiergo\Documents\GitHub\xiergo.github.io /eiy
+xcopy D:\Users\xiergo\Documents\GitHub\mysite-even\public D:\Users\xiergo\Documents\GitHub\xiergo.github.io /eiy
 cd D:\Users\xiergo\Documents\GitHub\xiergo.github.io
 git add .
 git commit -m 'gadf'
