3f9457539f9cd7dfa408c7d4740eff2ad9b262a2
xiergo <18811771366@163.com>
2022-11-12 21:14:09 +0800

    first commit

--- auto-update.bat
+++
@@ -1,9 +0,0 @@
-cd "D:\Users\xiergo\Documents\GitHub\mysite-even"
-hugo
-xcopy D:\Users\xiergo\Documents\GitHub\mysite-even\public D:\Users\xiergo\Documents\GitHub\xiergo.github.io /eiy
-cd D:\Users\xiergo\Documents\GitHub\xiergo.github.io
-git add .
-git commit -m 'gadf'
-git push
-
-pause
